4c436d504f044647217a28c12940c100348c1533
ehrvayn <rjmomong@gmail.com>
2026-03-01 19:14:27 +0800

    hosted

--- Back-end/src/database/Connection.js
+++ Back-end/src/database/Connection.js
@@ -2,18 +2,17 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect((err) => {
   if (err) {
     console.log("DB Connection Error: ", err);
   } else {
-    console.log("DB Connected!");
+    console.log("DB Connected to Supabase!");
   }
 });
 
